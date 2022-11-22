b4e9e179d298d462aa3ab454bd13c17f5bdf8eea
viniciusflyssak <viniciusflyssak@gmail.com>
2022-11-21 21:14:40 -0300

    First commit

---
+++ projGeraEtl.dpr
@@ -0,0 +1,14 @@
+program projGeraEtl;
+
+uses
+  Vcl.Forms,
+  uFrmPrincipal in 'uFrmPrincipal.pas' {FrmPrincipal};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TFrmPrincipal, FrmPrincipal);
+  Application.Run;
+end.
